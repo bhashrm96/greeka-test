3ff66075519eca628d7115bd53663cc34647dd23
Bhavya Sharma <kpnlms1@yahoo.com>
2025-07-25 12:12:06 +0530

    Vercel Config

---
+++ api/index.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+let cachedApp: any;
+
+async function bootstrap() {
+  const expressApp = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  await app.init();
+  return expressApp;
+}
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  try {
+    if (!cachedApp) {
+      cachedApp = await bootstrap();
+    }
+    cachedApp(req, res);
+  } catch (error) {
+    console.error('Server error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+}
